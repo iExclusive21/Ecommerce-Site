ec31432553239e06baddcbbe7e0877a9ca0c9f64
John Bynum <iexclusiveproductions@gmail.com>
2022-11-25 14:14:35 -0500

    index completed

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -11,10 +11,23 @@ Product.belongsTo(Category, {
 });
 
 // Categories have many Products
+Category.hasMany(Product,{
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
+});
 
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag,{
+  through: ProductTag,
+  foreignKey: "product_id",
+  onDelete: "CASCADE",
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product,{
+  through: ProductTag,
+  foreignKey: "tag_id",
+  onDelete: "CASCADE",
+});
 
 module.exports = {
   Product,
